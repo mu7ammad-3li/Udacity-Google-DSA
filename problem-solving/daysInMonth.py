1715eb6deda3d1f024d6e66d8224ecf33c9265da
Muhammad Ali <Mu7ammad.3lii@gmail.com>
2024-03-27 01:43:48 +0200

    modified daysInMonth(Year,Month) to account For leap years

--- problem-solving/daysInMonth.py
+++ problem-solving/daysInMonth.py
@@ -1,13 +1,18 @@
+from isLeapYear import isLeapYear
 
 # write stub **daysInMonth(Year,Month)** that Always Return 30 -- Done 
 # Modify daysInMonth(Year,Month) to be correct except for leep years --Done
+
 
 def daysInMonth(Year,Month):
     match Month:
         case 1:
             return 31
         case 2 :
-            return 28
+            if(isLeapYear(Year)):
+                return 29
+            else: 
+                return 28
         case 3 :
             return 31
         case 4 :
@@ -24,8 +29,8 @@ def daysInMonth(Year,Month):
             return 30
         case 10 :
             return 31
-        case 12 :
+        case 11 :
             return 30
-        case 13 :
+        case 12 :
             return 31
 

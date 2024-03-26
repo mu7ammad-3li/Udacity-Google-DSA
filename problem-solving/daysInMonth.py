c9943f8fe0c3901794083d15c342602136622c86
Muhammad Ali <Mu7ammad.3lii@gmail.com>
2024-03-27 01:10:39 +0200

    Modified daysInMonth(Year,Month)  to be correct except for leep years

--- problem-solving/daysInMonth.py
+++ problem-solving/daysInMonth.py
@@ -1,7 +1,31 @@
 
-# write stub **daysInMonth(Year,Month)** that Always Return 30 
-
-
+# write stub **daysInMonth(Year,Month)** that Always Return 30 -- Done 
+# Modify daysInMonth(Year,Month) to be correct except for leep years --Done
 
 def daysInMonth(Year,Month):
-    return 30
+    match Month:
+        case 1:
+            return 31
+        case 2 :
+            return 28
+        case 3 :
+            return 31
+        case 4 :
+            return 30
+        case 5 :
+            return 31
+        case 6 :
+            return 30
+        case 7 :
+            return 31
+        case 8 :
+            return 31
+        case 9 :
+            return 30
+        case 10 :
+            return 31
+        case 12 :
+            return 30
+        case 13 :
+            return 31
+
